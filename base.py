e373bd732df228fd9470739f0c64833b092632d5
Mc-Crispy <guapiskin@gmail.com>
2026-07-09 18:08:18 +0800

    Initial commit

---
+++ base.py
@@ -0,0 +1,29 @@
+api = input("请输入名字:")
+print(api)
+print(type(api))
+
+a = 10
+b = 3
+c = a + b
+print("a + b = " + str(c))
+c = a - b
+print("a - b = " + str(c))
+c = a * b
+print("a * b = " + str(c))
+c = a / b
+print("a / b = " + str(c))
+c = a // b
+print("a // b = " + str(c))#整除
+c = a % b
+print("a % b = " + str(c))#取余数
+c = a ** b
+print("a ** b = " + str(c))#次方运算
+
+num = int(input("请输入年龄:"))
+if(num < 18):
+    print("未成年人")
+elif(num >= 18 and num <= 50):
+    print("成年人和中年人")
+else:
+    print("老年人")
+
