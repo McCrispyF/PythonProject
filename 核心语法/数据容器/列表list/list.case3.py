40c2681da9ef65e01e7cca90e4f4d7bb3d1ce56e
Mc-Crispy <guapiskin@gmail.com>
2026-07-27 12:54:48 +0800

    daily update

---
+++ 核心语法/数据容器/列表list/list.case3.py
@@ -0,0 +1,27 @@
+#生成1-20的平方列表
+
+num_list1 = []
+for i in range(1,21):
+    num_list1.append(i ** 2)
+print (num_list1)
+
+
+num_list2 = [i ** 2 for i in range(1,21)]
+print (num_list2)
+
+
+#从一个数字列表中提取所有偶数，并计算其平方，组成一个新的列表
+#经典写法
+"""
+num_list = [12,44,13,75,24,61,6,53,22]
+new_list = []
+for i in num_list:
+    if i % 2 == 0:
+        new_list.append(i ** 2)
+print(new_list)
+"""
+
+#列表推导式
+num_list = [12,44,13,75,24,61,6,53,22]
+new_list = [i ** 2 for i in num_list if i % 2 == 0]
+print(new_list)
